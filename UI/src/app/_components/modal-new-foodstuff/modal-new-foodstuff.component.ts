a61ec239a31a6e8a6736496a6fee82e9a99ed735
Matteo Gheza <matteo.gheza07@gmail.com>
2021-09-27 22:11:23 +0200

    Better modal design

--- UI/src/app/_components/modal-new-foodstuff/modal-new-foodstuff.component.ts
+++ UI/src/app/_components/modal-new-foodstuff/modal-new-foodstuff.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ApiClientService } from '../../_services/api-client.service';
+import { DataLoaderService } from 'src/app/_services/data-loader.service';
 import { ModalNewFoodstuffTypeComponent } from '../modal-new-foodstuff-type/modal-new-foodstuff-type.component';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
@@ -10,10 +11,19 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ModalNewFoodstuffComponent implements OnInit {
   newFoodstuffForm = new FormGroup({
-    name: new FormControl('', Validators.required)
+    name: new FormControl('', Validators.required),
+    shortName: new FormControl(''),
+    description: new FormControl(''),
+    price: new FormControl('', Validators.required),
+    foodstuffType: new FormControl('', Validators.required)
   });
 
-  constructor(public activeModal: NgbActiveModal, private modalService: NgbModal, private api: ApiClientService) {}
+  constructor(
+    public activeModal: NgbActiveModal,
+    private modalService: NgbModal,
+    private api: ApiClientService,
+    public data: DataLoaderService
+  ) {}
 
   ngOnInit() {}
 
